fe593c92c28f50bf770e120572d4ef3ca957f7ec
ijokpy <>
2023-12-05 21:18:11 +0000

    Installed pyfiglet module and created ASCII Art Text banner

--- run.py
+++ run.py
@@ -2,6 +2,9 @@
 import gspread
 from google.oauth2.service_account import Credentials #to handle authentication
 
+#pip3 install pyfiglet
+import pyfiglet
+
 #Define list of scopes that defines the access levels app requires
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -9,22 +12,22 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
+#Constant variables
 #Load the service account credentials from the 'creds.json' file
 CREDS = Credentials.from_service_account_file('creds.json')
-
 #Using 'with_scopes' method to add specified OAuth 2.0 scopes to the credentials
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-
 #Using 'gspread.authorize' method creating a client to interact with Google Sheets
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-
 #Opening google sheet
 SHEET = GSPREAD_CLIENT.open('doctor_booking')
+ascii_banner = pyfiglet.figlet_format("Pulse Clinic")
+print(ascii_banner)
 
-#Getting a worksheet from Google Sheet
-availability = SHEET.worksheet('Availability')
 
-#Retrieving all values from the 'Availability' worksheet to test API
-data = availability.get_all_values()
-
-print(data)
+#def main_page():
+   # """
+    #Loads Welcome page for Pulse Clinic doctor booking system
+   # Provides selection to make a booking or cancel existing booking
+   # Validates entries
+    #"""
