927c7edabc94ffb1c1c2681c0ee6a113eef53023
ijokpy <>
2023-12-06 02:43:58 +0000

    Defined get_consultants function to retrieve from google sheets and to add into list for the users to select. Implemented validation mechanizms

--- run.py
+++ run.py
@@ -12,6 +12,9 @@ from colored import fg, bg, attr
 import os
 from time import sleep
 
+#pip3 install pandas
+import pandas as pd #to covert the data to DataFrame
+
 #Define list of scopes that defines the access levels app requires
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -46,23 +49,54 @@ def clear_screen():
     os.system('cls') #For Windows
     os.system('clear') #For Linux
 
-def main_page():
+def get_consultants():
     """
-    Loads Welcome page for Pulse Clinic doctor booking system
-    Provides selection to make a booking or cancel existing booking
-    Validates entries
+    Retrieve specialists that are available for booking
     """
-    def menu_options():
-        """
-        Function loads 1 options for the user to select and validates an input.
-        """
-        print(ascii_banner)
-        print("Welcome to our patient portal!")
-        print("Please select option 1 or 2 from the menu below:\n")
-        print(f"{CYAN} 1 {R} - Book an Appointment")
-        print(f"{CYAN} 2 {R} - Cancel existing Appointment\n")
+    available_consultants = SHEET.worksheet("Availability").get_all_values()
+    df =pd.DataFrame(available_consultants[1:],columns=available_consultants[0])
+    doctor_names = list(df.columns[1:])
+    for i, doctor_name in enumerate(doctor_names, start=1):
+        print(f"{CYAN}{i}{R}  -  {doctor_name}")
+    return doctor_names
 
-    menu_options()
+def book_appointment():
+    """
+    Function provides instructions how to book an appointment.
+    Provides Doctor/specialty options to select.
+    Provides available dates to select.
+    Provides available times to select.
+    Confirms booking.
+    Validates entries.
+    """
+    
+    print(f"{BLUE}NEW APPONTMENT BOOKING!\n")
+    print("Please select required specialist from the options below:\n")
+    doctor_names=get_consultants()
+    while True:
+        
+        try:
+            user_selection = int(input(f"{CYAN}Your selection: {R}\n"))
+            if user_selection in (1, 2, 3, 4, 5):
+                print(f"You selected {doctor_names[user_selection - 1]}")
+                break
+            else:
+                print(f"{RED}Invalid selection!{R}\n")
+                print(f"{RED}Please select a number{R}\n")
+        except ValueError:
+            menu_options()
+            sleep(4)
+            print(f"{RED}Invalid selection!{R}\n")
+
+def menu_options():
+    """
+    Function loads two options for the user to select and validates an input.
+    """
+    print(ascii_banner)
+    print("Welcome to our patient portal!")
+    print("Please select option 1 or 2 from the menu below:\n")
+    print(f"{CYAN} 1 {R} - Book an Appointment")
+    print(f"{CYAN} 2 {R} - Cancel existing Appointment\n")
     while True:
         user_selection = 0
         try:
@@ -84,7 +118,24 @@ def main_page():
             sleep(4)
             print(f"{RED}Invalid selection!{R}\n")
             print(f"{RED}Please select number 1 or 2{R}\n")
+
+
+
+
+
+ #def get_months():
+   # """
+   # Retrieve months that bookings can be made for.
+#
+   # """
+     #available_months =  SHEET.worksheet("Availability").get_all_values()    
+
+def main_page():
+    """
+    Loads Welcome page for Pulse Clinic doctor booking system
+    Provides selection to make a booking or cancel existing booking
+    Validates entries
+    """
+    menu_options()
+    
 main_page()
-
-        
-
